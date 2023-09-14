150c4851ab9a1b0c79f29706a7305d96c46dd70b
Gautham Sajith <gsajith@umich.edu>
2023-09-14 17:00:26 -0400

    Scanning selection for image fills and saving it in state

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,44 +1,49 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
+import React, { useState } from 'react';
 import '../styles/ui.css';
 
 function App() {
-  const textbox = React.useRef<HTMLInputElement>(undefined);
-
-  const countRef = React.useCallback((element: HTMLInputElement) => {
-    if (element) element.value = '5';
-    textbox.current = element;
-  }, []);
-
-  const onCreate = () => {
-    const count = parseInt(textbox.current.value, 10);
-    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*');
-  };
+  const [imageMap, setImageMap] = useState(null);
+  const [scanningSelection, setScanningSelection] = useState(false);
 
   const onCancel = () => {
     parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
+  };
+
+  const onCompress = () => {
+    parent.postMessage({ pluginMessage: { type: 'start-compress', imageMap } }, '*');
   };
 
   React.useEffect(() => {
     // This is how we read messages sent from the plugin controller
     window.onmessage = (event) => {
       const { type, message } = event.data.pluginMessage;
-      if (type === 'create-rectangles') {
+      if (type === 'compress-images') {
         console.log(`Figma Says: ${message}`);
+      } else if (type === 'selected-images') {
+        console.log('Got end');
+        setImageMap(message);
+        setScanningSelection(false);
+      } else if (type === 'start-selection') {
+        console.log('Got start');
+        setScanningSelection(true);
+        setTimeout(() => {
+          parent.postMessage({ pluginMessage: { type: 'start-scan' } }, '*');
+        }, 250);
       }
     };
   }, []);
 
   return (
     <div>
-      <img src={logo} />
-      <h2>Rectangle Creator</h2>
-      <p>
-        Count: <input ref={countRef} />
-      </p>
-      <button id="create" onClick={onCreate}>
-        Create
-      </button>
+      <h2>Image Compressor</h2>
+      <p>{scanningSelection && 'Scanning selection...'}</p>
+      <p>Images in selection: {imageMap ? Object.keys(imageMap).length : 0}</p>
+
+      {imageMap && Object.keys(imageMap).length > 0 && (
+        <button id="compress" onClick={onCompress}>
+          Compress
+        </button>
+      )}
       <button onClick={onCancel}>Cancel</button>
     </div>
   );
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,26 +1,100 @@
+import isGif from 'is-gif';
+
 figma.showUI(__html__);
 
-figma.ui.onmessage = (msg) => {
-  if (msg.type === 'create-rectangles') {
-    const nodes = [];
-
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+const findImages = (selection, result) => {
+  selection.forEach((item) => {
+    if (item.fills && item.fills.length > 0) {
+      item.fills.every((fill) => {
+        if (fill.type === 'IMAGE') {
+          result.push(item);
+          return false;
+        }
+        return true;
+      });
     }
+    if (typeof item.children !== 'undefined') {
+      findImages(item.children, result);
+    }
+  });
+};
 
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-
-    // This is how figma responds back to the ui
+figma.on('selectionchange', () => {
+  if (figma.currentPage.selection.length > 0) {
     figma.ui.postMessage({
-      type: 'create-rectangles',
-      message: `Created ${msg.count} Rectangles`,
+      type: 'start-selection',
+    });
+  } else {
+    figma.ui.postMessage({
+      type: 'selected-images',
+      message: null,
     });
   }
+});
 
-  figma.closePlugin();
+const startScan = () => {
+  let res = [];
+  findImages(figma.currentPage.selection, res);
+  filterOutGifs(res);
+};
+
+const filterOutGifs = (imageNodes) => {
+  let imageFills = [];
+  let imageHashToNodeMap = {};
+
+  imageNodes.forEach((node) => {
+    imageFills = imageFills.concat(node.fills);
+    node.fills.forEach((fill) => {
+      if (typeof fill.imageHash !== 'undefined') {
+        if (fill.imageHash in imageHashToNodeMap) {
+          imageHashToNodeMap[fill.imageHash][node.id] = true;
+        } else {
+          imageHashToNodeMap[fill.imageHash] = {
+            [node.id]: true,
+          };
+        }
+      }
+    });
+  });
+
+  // TODO: This is still throwing a ton of errors, skip this?
+  // const allImageHashes = Object.keys(imageHashToNodeMap);
+  // const gifImageHashes = [];
+
+  // for (let i = 0; i < allImageHashes.length; i++) {
+  //   const image = figma.getImageByHash(allImageHashes[i]);
+  //   if (image !== null) {
+  //     const bytes = await image.getBytesAsync();
+  //     if (isGif(bytes)) {
+  //       gifImageHashes.push(allImageHashes[i]);
+  //     }
+  //   }
+  // }
+
+  // for (let i = 0; i < gifImageHashes.length; i++) {
+  //   delete imageHashToNodeMap[gifImageHashes[i]];
+  // }
+
+  figma.ui.postMessage({
+    type: 'selected-images',
+    message: imageHashToNodeMap,
+  });
+};
+
+figma.ui.onmessage = async (msg) => {
+  if (msg.type === 'compress-images') {
+    figma.ui.postMessage({
+      type: 'compress-images',
+      message: `Compressed ${msg.count} images`,
+    });
+  } else if (msg.type === 'filter-out-gifs') {
+    filterOutGifs(msg.imageNodes);
+  } else if (msg.type === 'start-scan') {
+    startScan();
+  } else if (msg.type === 'start-compress') {
+    const imageMap = msg.imageMap;
+    console.log('Will compress...', imageMap);
+  }
+
+  // figma.closePlugin();
 };
